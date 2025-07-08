7570f206736e91dc2fdf237269e6cd23fc81cfdd
NicholasClooney <137233165+NicholasClooney@users.noreply.github.com>
2025-07-08 12:12:32 +1000

    Add `invoke lint` to run both swiftlint and swiftformat
    with the needed arguments

---
+++ tasks.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+
+
+from invoke import task
+
+@task
+def lint(c):
+    root = Path(__file__).parent
+
+    # print(root)
+
+    def plugin_command(plugin):
+        return f"swift package plugin --allow-writing-to-package-directory {plugin}"
+
+    build = "swift build"
+    folders = "Sources Tests"
+    swift_lint = plugin_command(f"swiftlint {folders}")
+    swift_format = plugin_command(f"swiftformat --swiftversion 6.1.2 {folders}")
+
+    commands = [
+            build,
+            swift_lint,
+            swift_format,
+    ]
+
+    # print(commands)
+
+    for command in commands:
+        c.run(command)
+        print()
