cfc91576d21a3a8439a940a534f24388369d04e1
NicholasClooney <137233165+NicholasClooney@users.noreply.github.com>
2025-07-08 11:54:14 +1000

    Wrangling with SPM
    Turns out SPM does not support per target platforms specification.
    Since we need to use Combine, let's just set `platforms` to the minimum
    Combine requires.

--- Package.swift
+++ Package.swift
@@ -5,29 +5,41 @@ import PackageDescription
 
 let package = Package(
     name: "TinyObjects",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v10_15),
+        .tvOS(.v13),
+        .visionOS(.v1),
+        .watchOS(.v6)
+    ],
     products: [
         // Products define the executables and libraries a package produces
         // making them visible to other packages.
         .library(
             name: "TinyObjects",
-            targets: ["TinyObjects"]),
+            targets: ["TinyObjects"]
+        ),
     ],
     dependencies: [
         // Quick and Nimble for testing
         .package(url: "https://github.com/Quick/Quick.git", from: "7.6.2"),
-        .package(url: "https://github.com/Quick/Nimble.git", from: "13.7.1"),
+
+        // SwiftLintPlugins for linting using SPM Build Tool Plugin
+        // See the README for reasons to use the dedecated plugins repo rather than SwiftLint repo.
+        .package(url: "https://github.com/SimplyDanny/SwiftLintPlugins.git", from: "0.59.1"),
+
+        // SwiftFormat for formatting using SPM Build Tool Plugin
+        .package(url: "https://github.com/nicklockwood/SwiftFormat.git", from: "0.56.4"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "TinyObjects"),
+            name: "TinyObjects"
+        ),
         .testTarget(
             name: "TinyObjectsTests",
             dependencies: [
                 "TinyObjects",
-                .product(name: "Quick", package: "Quick"),
-                .product(name: "Nimble", package: "Nimble"),
+                "Quick",
             ]
         ),
     ]
