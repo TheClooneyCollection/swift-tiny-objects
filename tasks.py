742f81ccd338f6fdaae9609d7e560dbcc81ec4f3
NicholasClooney <137233165+NicholasClooney@users.noreply.github.com>
2025-07-17 21:12:23 +1000

    Add an `inovke format` task

--- tasks.py
+++ tasks.py
@@ -1,21 +1,47 @@
 from pathlib import Path
 
-
 from invoke import task
+
 
 @task
 def test(c):
     command = "swift test"
     c.run(command)
 
+
+def plugin_command(plugin):
+    return f"swift package plugin --allow-writing-to-package-directory {plugin}"
+
+
+@task
+def format(c):
+    root = Path(__file__).parent
+
+    # print(root)
+
+    build = "swift build"
+    folders = "Sources Tests"
+    swift_lint = plugin_command(f"swiftlint --fix {folders}")
+    swift_format = plugin_command(f"swiftformat --swiftversion 6.1.2 {folders}")
+
+    commands = [
+            build,
+            swift_lint,
+            swift_format,
+    ]
+
+    # print(commands)
+
+    for command in commands:
+        c.run(command)
+        print()
+
+
 @task
 def lint(c):
     root = Path(__file__).parent
 
     # print(root)
-
-    def plugin_command(plugin):
-        return f"swift package plugin --allow-writing-to-package-directory {plugin}"
 
     build = "swift build"
     folders = "Sources Tests"
