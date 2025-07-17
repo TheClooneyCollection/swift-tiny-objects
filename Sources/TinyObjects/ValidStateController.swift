5ea6154909704898f095851f7ef4b939836894da
NicholasClooney <137233165+NicholasClooney@users.noreply.github.com>
2025-07-17 21:09:22 +1000

    Add the initial value loading of ValidStateController and tests

---
+++ Sources/TinyObjects/ValidStateController.swift
@@ -0,0 +1,86 @@
+//
+//  ValidStateController.swift
+//  TinyObjects
+//
+//  Created by Nicholas Clooney on 17/7/2025.
+//
+
+public actor ValidStateController<Value: Equatable & Sendable, Error: Swift.Error>: Sendable {
+    /// Returns `Value` when a value is valid.
+    /// Returns `nil` when a value is no longer valid.
+    public typealias Validate = (Value) -> Value?
+
+    public typealias WorkCompletionHandler = (Result<Value, Error>) -> Void
+    public typealias Work = (
+        _ completion: @escaping WorkCompletionHandler
+    ) -> Void
+
+    public struct Storage: Sendable {
+        public let load: @Sendable () -> Value?
+        public let save: @Sendable (Value) -> Void
+
+        public init(
+            load: @escaping @Sendable () -> Value?,
+            save: @escaping @Sendable (Value) -> Void
+        ) {
+            self.load = load
+            self.save = save
+        }
+    }
+
+    public enum State: Equatable & Sendable {
+        case initial
+        case workInProgress
+        case valid(Value)
+        case invalid
+
+//        public enum Invalid {
+//            case invalidated
+//            case timedOut
+//            case cancelled
+//            case failed(Error)
+//        }
+    }
+
+    public private(set) var state: State
+
+    private let work: Work
+    private let storage: Storage
+    private let validate: Validate
+
+    public init(
+        work: @escaping Work,
+        storage: Storage,
+        validate: @escaping Validate
+    ) {
+        self.work = work
+        self.storage = storage
+        self.validate = validate
+
+        state = .initial
+    }
+
+    private func loadState() {
+        // TODO: should try fetching a new one
+        guard let storedValue = storage.load() else {
+            state = .invalid
+
+            return
+        }
+        guard let validValue = validate(storedValue) else {
+            state = .invalid
+            return
+        }
+
+        state = .valid(validValue)
+    }
+
+    public func update(state _: State) {}
+
+    public func requestRefresh() {}
+
+    public func forceRefresh() {}
+
+    // ???
+    public func cancelRefresh() {}
+}
---
+++ Tests/TinyObjectsTests/ValidStateControllerSpecs/ValidStateControllerInitialLoadingSpec.swift
@@ -0,0 +1,80 @@
+//
+//  ValidStateControllerInitialLoadingSpec.swift
+//  TinyObjects
+//
+//  Created by Nicholas Clooney on 17/7/2025.
+//
+
+import Testing
+
+import Quick
+
+import TinyObjects
+
+final class ValidStateControllerInitialLoadingSpec: AsyncSpec {
+    override class func spec() {
+        typealias Controller = ValidStateController<Int, Never>
+
+        var controller: Controller!
+        var storage: Controller.Storage!
+
+        context("when there is a valid value in the storage") {
+            beforeEach {
+                storage = .init(load: { 42 }, save: { _ in })
+            }
+
+            context("when starting the controller") {
+                beforeEach {
+                    controller = .init(
+                        work: { handler in handler(.success(42))},
+                        storage: storage,
+                        validate: { if $0 == 42 { 42 } else { nil } })
+                }
+
+                it("has an invalid state") {
+                    let state = await controller.state
+                    #expect(state == .valid(42))
+                }
+            }
+
+        }
+
+        context("when there is an invalid value in the storage") {
+            beforeEach {
+                let not42 = 41
+                storage = .init(load: { not42 }, save: { _ in })
+            }
+
+            context("when starting the controller") {
+                beforeEach {
+                    controller = .init(
+                        work: { handler in handler(.success(42))},
+                        storage: storage,
+                        validate: { if $0 == 42 { 42 } else { nil } })
+                }
+
+                it("has an invalid state") {
+                    let state = await controller.state
+                    #expect(state == .invalid)
+                }
+            }
+        }
+
+        context("when there is nothing in the storage") {
+            beforeEach {
+                storage = .init(load: { nil }, save: { _ in })
+            }
+
+            context("when starting the controller") {
+                beforeEach {
+                    controller = .init(work: { handler in handler(.success(42)) }, storage: storage, validate: { $0 })
+                }
+
+                it("has an invalid state") {
+                    let state = await controller.state
+                    #expect(state == .invalid)
+                }
+            }
+        }
+    }
+}
