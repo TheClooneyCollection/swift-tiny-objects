337bc527ffd23c5de221833495e9f4836912d618
NicholasClooney <137233165+NicholasClooney@users.noreply.github.com>
2025-07-18 18:40:33 +1000

    Use Nimble's `expect` and reformat

--- Tests/TinyObjectsTests/ValidStateControllerSpecs/ValidStateControllerInitialLoadingSpec.swift
+++ Tests/TinyObjectsTests/ValidStateControllerSpecs/ValidStateControllerInitialLoadingSpec.swift
@@ -5,9 +5,8 @@
 //  Created by Nicholas Clooney on 17/7/2025.
 //
 
-import Testing
-
 import Quick
+import Nimble
 
 import TinyObjects
 
@@ -18,66 +17,72 @@ final class ValidStateControllerInitialLoadingSpec: AsyncSpec {
         var controller: Controller!
         var storage: Controller.Storage!
 
-        context("when there is a valid value in the storage") {
-            beforeEach {
-                storage = .init(load: { 42 }, save: { _ in })
-            }
-
-            context("when starting the controller") {
+        describe("ValidStateController") {
+            context("when there is a valid value in the storage") {
                 beforeEach {
-                    controller = .init(
-                        work: { handler in handler(.success(42)) },
-                        storage: storage,
-                        validate: { if $0 == 42 { 42 } else { nil } },
-                    )
+                    storage = .init(load: { 42 }, save: { _ in })
                 }
 
-                it("has an invalid state") {
-                    let state = await controller.state
-                    #expect(state == .valid(42))
+                context("when starting the controller") {
+                    beforeEach {
+                        controller = .init(
+                            work: { handler in handler(.success(42)) },
+                            storage: storage,
+                            validate: { if $0 == 42 { 42 } else { nil } },
+                        )
+                    }
+
+                    it("has an invalid state") {
+                        let state = await controller.state
+
+                        expect(state) == .valid(42)
+                    }
                 }
             }
-        }
 
-        context("when there is an invalid value in the storage") {
-            beforeEach {
-                let not42 = 41
-                storage = .init(load: { not42 }, save: { _ in })
-            }
-
-            context("when starting the controller") {
+            context("when there is an invalid value in the storage") {
                 beforeEach {
-                    controller = .init(
-                        work: { handler in handler(.success(42)) },
-                        storage: storage,
-                        validate: { if $0 == 42 { 42 } else { nil } },
-                    )
+                    let not42 = 41
+
+                    storage = .init(load: { not42 }, save: { _ in })
                 }
 
-                it("has an invalid state") {
-                    let state = await controller.state
-                    #expect(state == .invalid)
+                context("when starting the controller") {
+                    beforeEach {
+                        controller = .init(
+                            work: { handler in handler(.success(42)) },
+                            storage: storage,
+                            validate: { if $0 == 42 { 42 } else { nil } },
+                        )
+                    }
+
+                    it("has an invalid state") {
+                        let state = await controller.state
+
+                        expect(state) == .invalid
+                    }
                 }
             }
-        }
 
-        context("when there is nothing in the storage") {
-            beforeEach {
-                storage = .init(load: { nil }, save: { _ in })
-            }
-
-            context("when starting the controller") {
+            context("when there is nothing in the storage") {
                 beforeEach {
-                    controller = .init(
-                        work: { handler in handler(.success(42)) },
-                        storage: storage,
-                        validate: { $0 },
-                    )
+                    storage = .init(load: { nil }, save: { _ in })
                 }
 
-                it("has an invalid state") {
-                    let state = await controller.state
-                    #expect(state == .invalid)
+                context("when starting the controller") {
+                    beforeEach {
+                        controller = .init(
+                            work: { handler in handler(.success(42)) },
+                            storage: storage,
+                            validate: { $0 },
+                        )
+                    }
+
+                    it("has an invalid state") {
+                        let state = await controller.state
+
+                        expect(state) == .invalid
+                    }
                 }
             }
         }
